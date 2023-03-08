76e7996b2435f4036cc8d3b2398d32944dd1b60a
NootNook <67638224+NootNook@users.noreply.github.com>
2023-03-08 15:44:27 +0100

    init project

---
+++ app/src/main/java/com/retina_uav/tracker_poi_ar/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.retina_uav.tracker_poi_ar
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
